3762adda3c01b32b7accf46bd364557c593684b5
bubusaurio <charritos33@hotmail.com>
2025-06-17 02:19:46 -0600

    Refactor Vite configuration by removing unused environment variable loading and server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
 
-export default defineConfig(({ mode }) => {
-  // Load env variables
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [
-      react(), tailwindcss()
-    ],
-    server: {
-      host: '192.168.68.119',
-      https: {
-        key: fs.readFileSync(env.VITE_HTTPS_KEY),
-        cert: fs.readFileSync(env.VITE_HTTPS_CERT),
-      },
-    },
-  }
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(), tailwindcss()
+  ],
 })
 
 
